b03b42ce016fe9ef7448f2774c656a2f2fb9789c
CrazyElephantX <79636902266@ya.ru>
2021-03-23 17:34:15 +0300

    разбор с выводом на экран

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+)
 
 func main() {
- fmt.Println("Всем привет, это скрипт.")
- fmt.Println("Всем пока, программа завершена.")
+
+	currentTime := time.Now()
+
+	type passport struct {
+		series string
+		number string
+	}
+
+	//passports := make([]passport, 0)
+	file, err := ioutil.ReadFile("list_of_expired_passports_1.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	passportline := strings.Split(string(file), "\n")
+
+	for i := 0; i < len(passportline); i++ {
+
+		if passportline[i] != "" {
+
+			CurrentPassport := strings.Split(string(passportline[i]), ",")
+
+			newPassport := passport{series: CurrentPassport[0], number: CurrentPassport[1]}
+			//passports = append(passports, newPassport)
+			fmt.Println(newPassport.series + newPassport.number)
+		}
+	}
+
+	currentTime2 := time.Now()
+	fmt.Println(currentTime)
+	fmt.Println(currentTime2)
+
 }
