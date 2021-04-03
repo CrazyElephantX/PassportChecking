d349408428173cefb4370a579eaeae091d08482e
CrazyElephantX <79636902266@ya.ru>
2021-04-03 03:12:36 +0300

    Переделан способ писать в базу, пишем сразу все данные напрямую из файла

--- main.go
+++ main.go
@@ -2,32 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"encoding/csv"
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
-	"os"
 	"time"
 )
 
 func main() {
 
 	currentTime := time.Now()
-
-	/*type passport struct {
-		series string
-		number string
-	}*/
-
-	//Способ 2, чтение файла CSV
-	file, err := os.Open("list_of_expired_passports.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	reader.FieldsPerRecord = 2
 
 	password := ""
 	connStr := "user=postgres password=" + password + " dbname=passport_DB sslmode=disable"
@@ -36,27 +19,11 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	for {
-		CurrentPassport, e := reader.Read()
-		if e != nil {
-			fmt.Println(e)
-			break
-		}
-		//newPassport := passport{series: CurrentPassport[0], number: CurrentPassport[1]}
-		//passports = append(passports, newPassport)
-		//fmt.Println(newPassport.series + " " + newPassport.number)
-		if CurrentPassport[0] != "PASSP_SERIES" {
-			//result, err := db.Exec("insert into docs (pas_series, pas_number) values ($1,$2)",CurrentPassport[0], CurrentPassport[1])
-			db.Exec("insert into docs (pas_series, pas_number) values ($1,$2)", CurrentPassport[0], CurrentPassport[1])
-			/* if err != nil{
-				panic(err)
-			}
-			fmt.Println(result) */
 
-		}
-
+	db.Exec("COPY docs FROM 'C:/12/ft.csv' WITH CSV HEADER")
+	if err != nil {
+		panic(err)
 	}
-
 	currentTime2 := time.Now()
 	fmt.Println(currentTime)
 	fmt.Println(currentTime2)
